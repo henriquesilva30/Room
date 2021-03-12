1979d56897b2ebc813293646a6fb89e06689a8a8
Joel Silva <henriquesa@ipvc.pt>
2021-03-12 19:06:28 +0000

    Room inicio

---
+++ app/src/main/java/ipvc/estg/room/dao/WordDao.kt
@@ -0,0 +1,21 @@
+package ipvc.estg.room.dao
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import ipvc.estg.room.entities.Word
+
+@Dao
+interface WordDao {
+
+    @Query("SELECT * FROM word_table ORDER BY word ASC")
+    fun getAlphabetizedWords(): LiveData<List<Word>>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(word: Word)
+
+    @Query("DELETE FROM word_table")
+    suspend fun deleteAll()
+}
---
+++ app/src/main/java/ipvc/estg/room/db/WordRepository.kt
@@ -0,0 +1,24 @@
+package ipvc.estg.room.db
+
+import androidx.annotation.WorkerThread
+import androidx.lifecycle.LiveData
+import ipvc.estg.room.dao.WordDao
+import ipvc.estg.room.entities.Word
+
+// Declares the DAO as a private property in the constructor. Pass in the DAO
+// instead of the whole database, because you only need access to the DAO
+class WordRepository(private val wordDao: WordDao) {
+
+    // Room executes all queries on a separate thread.
+    // Observed Flow will notify the observer when the data has changed.
+    val allWords: LiveData<List<Word>> = wordDao.getAlphabetizedWords()
+
+    // By default Room runs suspend queries off the main thread, therefore, we don't need to
+    // implement anything else to ensure we're not doing long running database work
+    // off the main thread.
+    @Suppress("RedundantSuspendModifier")
+    @WorkerThread
+    suspend fun insert(word: Word) {
+        wordDao.insert(word)
+    }
+}
