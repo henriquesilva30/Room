4df72819a49b112f175d39d2a081a15b5de2c149
Joel Silva <henriquesa@ipvc.pt>
2021-03-18 18:14:50 +0000

    add RecyclerView

--- app/src/main/java/ipvc/estg/room/MainActivity.kt
+++ app/src/main/java/ipvc/estg/room/MainActivity.kt
@@ -2,12 +2,20 @@ package ipvc.estg.room
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import ipvc.estg.room.adapters.WordListAdapter
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerview)
+        val adapter = WordListAdapter()
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
 
     }
 }
